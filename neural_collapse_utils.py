66a8dc23775e8c6f34afb9f19f1b4e2cc388fbaa
Matthew Farrell <msfarrell@seas.harvard.edu>
2022-05-12 23:16:06 -0400

    sync

--- neural_collapse_utils.py
+++ neural_collapse_utils.py
@@ -1,10 +1,18 @@
 import timm
+from sklearn.svm import LinearSVC as Classifier
+from sklearn.exceptions import ConvergenceWarning
+import warnings
+warnings.filterwarnings("ignore", category=ConvergenceWarning)
+# from sklearn.linear_model import LogisticRegression as Classifier
 import torch
 from pathlib import Path
 from timm import data
 from timm import models
 from matplotlib import pyplot as plt
 from timm.models import create_model, safe_model_name, resume_checkpoint, load_checkpoint
+
+def vmean(v):
+    return sum(v) / len(v)
 
 def get_pcs_covariance(X, pcs, original_shape=True, return_extra=False):
     """
@@ -41,7 +49,51 @@ def get_pcs_covariance(X, pcs, original_shape=True, return_extra=False):
     else:
         return pca_proj
 
+def pairwise_class_dists(X, y, breakv=False):
+    cs = set(y.tolist())
+    m = len(cs)
+    ds = torch.zeros(m,m)
+    for k1 in range(m):
+        for k2 in range(k1, m):
+            xk1 = X[y == k1]
+            xk1 = xk1.reshape(1, xk1.shape[0], -1) 
+            xk2 = X[y == k2]
+            xk2 = xk2.reshape(1, xk2.shape[0], -1) 
+            d = torch.cdist(xk1, xk2)[0]
+            r1 = d.shape[0]
+            r2 = d.shape[1]
+            if k1 == k2:
+                ds[k1, k2] = torch.sum(torch.triu(d,1)) / ((r1-1)*(r2-1)/2)
+            else:
+                ds[k1, k2] = torch.sum(torch.triu(d,0)) / (r1*r2/2)
+    return ds 
+
 def within_over_across_class_mean_dist(X, y, breakv=False):
+    cs = set(y.tolist())
+    m = len(cs)
+    ds = torch.zeros(m,m)
+    for k1 in range(m):
+        for k2 in range(k1, m):
+            xk1 = X[y == k1]
+            xk1 = xk1.reshape(1, xk1.shape[0], -1) 
+            xk2 = X[y == k2]
+            xk2 = xk2.reshape(1, xk2.shape[0], -1) 
+            d = torch.cdist(xk1, xk2)[0]
+            r1 = d.shape[0]
+            r2 = d.shape[1]
+            if k1 == k2:
+                ds[k1, k2] = torch.sum(torch.triu(d,1)) / ((r1-1)*(r2-1)/2)
+            else:
+                ds[k1, k2] = torch.sum(torch.triu(d,0)) / (r1*r2/2)
+            # if ds[k1, k2] == torch.nan:
+                # breakpoint()
+            
+    d_within = torch.nanmean(torch.diag(ds))
+    d_across = torch.sum(torch.triu(ds, 1)) / ((m-1)**2/2)
+
+    return d_within.item(), d_across.item()
+
+def pairwise_dists_projected(X, y, breakv=False):
     cs = set(y.tolist())
     m = len(cs)
     ds = torch.zeros(m,m)
@@ -94,13 +146,6 @@ def get_compressions(feat_extractor, loader, run_dir, n_batches):
                           feat in features]
             within_layers = [o[0] for o in out_layers]
             across_layers = [o[1] for o in out_layers]
-            # lr = range(len(within_layers))
-            # zt = [h for h in within_layers if h != 0]
-            # within_layers = [within_layers[k] for k in lr if zt[k]]
-            # across_layers = [across_layers[k] for k in lr if zt[k]]
-            # within_layers = [h for h in within_layers if h != 0]
-            # zt = [w!=0 for w in within_layers]
-            # within_layers = []
             
             within_inputs += [within_layers]
             across_inputs += [across_layers]
@@ -109,11 +154,126 @@ def get_compressions(feat_extractor, loader, run_dir, n_batches):
     d_within = zip(*within_inputs)
     d_across = zip(*across_inputs)
 
-    d_within_avgs = torch.tensor([sum(d) / len(d) for d in d_within])
-    d_across_avgs = torch.tensor([sum(d) / len(d) for d in d_across])
+    d_within_avgs = torch.tensor([vmean(d) for d in d_within])
+    d_across_avgs = torch.tensor([vmean(d) for d in d_across])
     compression = d_within_avgs / d_across_avgs
     return compression
 
+def get_dists_projected(feat_extractor, loader, run_dir, n_batches):
+    data_size = len(loader)
+    feat_col = []
+    labels_col = []
+    ds_within_tot = []
+    ds_across_tot = []
+    ds_within_aligned_tot = []
+    ds_across_aligned_tot = []
+    ds_within_aligned_ratio_tot = []
+    ds_across_aligned_ratio_tot = []
+    inpdata = []
+    labels = []
+    out_inputs = []
+    within_inputs = []
+    across_inputs = []
+    print("Reminder to check layer orderings.")
+    for k1, (inpdata_batch, labels_batch) in enumerate(loader):
+        inpdata += [inpdata_batch]
+        labels += [labels_batch]
+        if (k1 + 1) % n_batches == 0:
+            inpdata = torch.cat(inpdata, dim=0)
+            labels = torch.cat(labels, dim=0).cpu()
+            # labels_pm1 = (2*labels - 1).cpu()
+            # features = feat_extractor(inpdata)
+            featt = feat_extractor(inpdata)
+            features = featt.values()
+            features = [feat.data.squeeze() for feat in features]
+            features_mc = [feat - torch.mean(feat, dim=0) for feat in features]
+            features_mc = [feat.reshape(feat.shape[0], -1).cpu() for feat in
+                           features_mc]
+            ds_within_layers = []
+            ds_across_layers = []
+            ds_within_aligned_layers = []
+            ds_across_aligned_layers = []
+            ds_within_aligned_ratio_layers = []
+            ds_across_aligned_ratio_layers = []
+            for k2, feat in enumerate(features_mc):
+                ds = pairwise_class_dists(feat, labels)
+                m = ds.shape[0]
+                ds_within_layers.append(
+                    (torch.nanmean(torch.diag(ds))).item())
+                ds_across_layers.append(
+                    (torch.sum(torch.triu(ds, 1))/((m-1)**2/2)).item())
+                classf = Classifier(max_iter=50, C=10)
+                classf.fit(feat.numpy(), labels.numpy())
+                w = torch.tensor(classf.coef_, dtype=torch.float)
+                b = torch.tensor(classf.intercept_, dtype=torch.float)
+                wn = w / torch.norm(w, dim=1, keepdim=True)
+                D = w.shape[1]
+                P_orths = torch.stack(
+                    [torch.eye(D) - torch.outer(w, w) for w in wn])
+                # feat_align = 
+                # out_layers = within_over_across_class_mean_dist(feat, labels)
+                feat_aligned = wn @ feat.T + b.unsqueeze(dim=1)
+                ds_within_aligned = []
+                ds_across_aligned = []
+                ds_within_aligned_ratio = []
+                ds_across_aligned_ratio = []
+                for k3 in range(m):
+                    ds_aligned = pairwise_class_dists(feat_aligned[k3], labels)
+                    ds_aligned_ratio = ds_aligned / ds
+                    ds_orth_ratio = 1 - ds_aligned_ratio
+                    ds_within_aligned.append(torch.nanmean(
+                        torch.diag(ds_aligned)).item())
+                    ds_across_aligned.append(
+                        (torch.sum(torch.triu(ds_aligned, 1))/((m-1)**2/2)).item())
+                    ds_within_aligned_ratio.append(torch.nanmean(
+                        torch.diag(ds_aligned_ratio)).item())
+                    ds_across_aligned_ratio.append(
+                        (torch.sum(torch.triu(ds_aligned_ratio, 1))/((m-1)**2/2)).item())
+                ds_within_aligned_layers.append(vmean(ds_within_aligned))
+                ds_across_aligned_layers.append(vmean(ds_across_aligned))
+                ds_within_aligned_ratio_layers.append(vmean(ds_within_aligned_ratio))
+                ds_across_aligned_ratio_layers.append(vmean(ds_across_aligned_ratio))
+
+            ds_within_tot.append(ds_within_layers)
+            ds_across_tot.append(ds_across_layers)
+            ds_within_aligned_tot.append(ds_within_aligned_layers)
+            ds_across_aligned_tot.append(ds_across_aligned_layers)
+            ds_within_aligned_ratio_tot.append(ds_within_aligned_ratio_layers)
+            ds_across_aligned_ratio_tot.append(ds_across_aligned_ratio_layers)
+
+            inpdata = []
+            labels = []
+    ds_within_tot = zip(*ds_within_tot)
+    ds_across_tot = zip(*ds_across_tot)
+    ds_within_aligned_tot = zip(*ds_within_aligned_tot)
+    ds_across_aligned_tot = zip(*ds_across_aligned_tot)
+    # d_within_orth_ratio_tot = 1 - ds_within_aligned_ratio_tot
+    # ds_across_orth_ratio_tot = 1 - ds_across_aligned_ratio_tot
+    ds_within_aligned_ratio_tot = zip(*ds_within_aligned_ratio_tot)
+    ds_across_aligned_ratio_tot = zip(*ds_across_aligned_ratio_tot)
+    # ds_within_orth_ratio_tot = zip(*ds_within_orth_ratio_tot)
+    # ds_across_orth_ratio_tot = zip(*ds_across_orth_ratio_tot)
+
+    ds_within_avgs = torch.tensor([vmean(d) for d in ds_within_tot])
+    ds_across_avgs = torch.tensor([vmean(d) for d in ds_across_tot])
+    ds_within_aligned_avgs = torch.tensor([vmean(d) for d in
+                                          ds_within_aligned_tot])
+    ds_across_aligned_avgs = torch.tensor([vmean(d) for d in
+                                          ds_across_aligned_tot])
+    ds_within_aligned_ratio_avgs = torch.tensor([vmean(d) for d in
+                                          ds_within_aligned_ratio_tot])
+    ds_across_aligned_ratio_avgs = torch.tensor([vmean(d) for d in
+                                          ds_across_aligned_ratio_tot])
+    # ds_within_orth_ratio_avgs = torch.tensor([vmean(d) for d in
+                                          # ds_orth_aligned_ratio_tot])
+    # ds_across_orth_ratio_avgs = torch.tensor([vmean(d) for d in
+                                          # ds_orth_aligned_ratio_tot])
+    # compression_avgs = ds_within_avgs / ds_across_avgs
+    # compression_aligned_ratio_avgs = d_within_aligned_ratio_tot / d_across_aligned_ratio_tot
+    # compression_orth_ratio_avgs = d_within_orth_ratio_tot / d_across_orth_ratio_tot 
+    return (ds_within_avgs, ds_across_avgs, ds_within_aligned_avgs,
+            ds_across_aligned_avgs, ds_within_aligned_ratio_avgs,
+            ds_across_aligned_ratio_avgs)
 # %% 
 # if __name__ == '__main__':
     
--- run_neural_collapse.py
+++ run_neural_collapse.py
@@ -29,7 +29,8 @@ parser_outer.add_argument('--run-num', type=int, default=None, metavar='N',
 args_outer, remaining_outer = parser_outer.parse_known_args()
 run_num = args_outer.run_num
 
-mem_cache = Path('.neural_collapse_cache')
+# mem_cache = Path('.neural_collapse_cache')
+mem_cache = Path('.neural_collapse_cache_old')
 memory = mom.Memory(location=mem_cache)
 # memory.clear()
 
@@ -68,23 +69,57 @@ outdir = exp.core_params['output']
     # return get_compressions(param_dict=param_dict, epoch=epoch,
                             # layer_ids=layer_ids, n_batches=n_batches,
                             # train_out=train_out)
+@memory.cache(ignore=['train_out'])
+def get_dists_projected(param_dict, epoch, layer_ids, n_batches=n_batches,
+                        n_samples=None, mode='val', train_out=None):
+    if mode != 'val' or n_samples is not None:
+        raise AttributeError('Not implemented yet')
+    model, loader_train, loader_val, run_dir, pd_mom = train_out
+    model.eval()
+    nodes, __ = fe.get_graph_node_names(model)
+    load_utils.load_model_from_epoch_and_dir(model, run_dir, epoch)
+    model.eval()
+    feature_dict = {}
+    node_range = list(range(len(nodes)))
+    node_range_dict = {key: val for key, val in zip(nodes, node_range)}
+    if isinstance(layer_ids, slice):
+        layer_ids = node_range[layer_ids]
+    layer_id_conv = []
+    nodes_filt = []
+    for layer_id in layer_ids:
+        if isinstance(layer_id, str):
+            if layer_id not in nodes:
+                raise ValueError("layer_id not valid.")
+            feature_dict[layer_id] = layer_id
+            layer_id_conv.append(node_range_dict[layer_id])
+            nodes_filt.append(layer_id)
+        elif isinstance(layer_id, int):
+            layer_key = nodes[layer_id]
+            feature_dict[layer_key] = layer_key
+            layer_id_conv.append(node_range[layer_id])
+            nodes_filt.append(nodes[layer_id])
+    feat_extractor = fe.create_feature_extractor(model, return_nodes=feature_dict)
+    # compression_train = utils.get_compressions_projected(
+        # feat_extractor, loader_train, run_dir, n_batches)
+    dists = utils.get_dists_projected(
+        feat_extractor, loader_val, run_dir, n_batches)
+    # (d_within_avgs, d_across_avgs, d_within_aligned_avgs,
+                 # d_across_aligned_avgs, d_within_aligned_ratio_avgs,
+                 # d_across_aligned_ratio_avgs) = out
+    return dists, layer_id_conv, nodes_filt
+
 
 @memory.cache(ignore=['train_out'])
 def get_compressions(param_dict, epoch, layer_ids, n_batches=n_batches,
                      train_out=None):
-        # mem_name = 'get_compressions'
-        # run_dir = mem_cach/mom.get_run_hash(param_dict)
         model, loader_train, loader_val, run_dir, pd_mom = train_out
         model.eval()
         nodes, __ = fe.get_graph_node_names(model)
-        # feature_dict = {key: key for key in nodes}
-        # feature_dict_lays = feature_dict.copy()
         load_utils.load_model_from_epoch_and_dir(model, run_dir, epoch)
         model.eval()
         feature_dict = {}
         node_range = list(range(len(nodes)))
-        node_range_dict = {key: val for key, val in
-                           zip(nodes, node_range)}
+        node_range_dict = {key: val for key, val in zip(nodes, node_range)}
         if isinstance(layer_ids, slice):
             layer_ids = node_range[layer_ids]
         layer_id_conv = []
@@ -108,15 +143,10 @@ def get_compressions(param_dict, epoch, layer_ids, n_batches=n_batches,
                                                 run_dir, n_batches)
         return compression_train, compression_val, layer_id_conv, nodes_filt
 
-# @memory.cache
-def get_compressions_over_training_cached(param_dict, epochs_idx=None,
-                                          layer_id=-1, n_batches=n_batches):
-    return get_compressions_over_training(param_dict, epochs_idx=None,
-                                          layer_id=layer_id,
-                                          n_batches=n_batches)
 
 def get_compressions_over_training(param_dict, epochs_idx=None, layer_id=-1,
-                                   n_batches=n_batches):
+                                   n_batches=n_batches, projection=None,
+                                   n_samples=None, mode='val'):
     train_out = train.train(param_dict)
     model, loader_train, loader_val, run_dir, pd_mom = train_out
     epochs = np.array(load_utils.get_epochs(run_dir))
@@ -124,29 +154,32 @@ def get_compressions_over_training(param_dict, epochs_idx=None, layer_id=-1,
         epochs = epochs[epochs_idx]
     ds = []
     for k1, epoch in enumerate(epochs):
-        # out = get_compressions_cached(
-            # param_dict, epoch, [layer_id], n_batches, train_out)
-        out = get_compressions(
-            param_dict, epoch, [layer_id], n_batches, train_out)
-        compression_train, compression_val, layer_id_k1, name_k1 = out
-        layer_id_k1 = layer_id_k1[0]
-        name_k1 = name_k1[0]
-        compression_train = compression_train[0].tolist()
-        compression_val = compression_val[0].tolist()
-        d = {'epoch': epoch, 'compression': compression_train, 
-             'layer_idx': layer_id_k1, 'layer_name': name_k1,
-             'mode': 'train'}
-        d = {**d, **pd_mom}
-        ds.append(d)
-        d = {'epoch': epoch, 'compression': compression_val, 
-             'layer_idx': layer_id_k1, 'layer_name': name_k1,
-             'mode': 'val'}
-        d = {**d, **pd_mom}
-        ds.append(d)
+        if projection is not None:
+            out = get_dists_projected(param_dict, epoch, [layer_id],
+                                      n_batches, n_samples, mode, train_out)
+            dists, layer_id_k1, name_k1 = out
+            compression = (dists[0] / dists[1]).item()
+            d = {'epoch': epoch, 'compression': compression, 
+                 'layer_idx': layer_id_k1[0], 'layer_name': name_k1[0],
+                 'mode': mode}
+            d = {**d, **pd_mom}
+            ds.append(d)
+        else:
+            out = get_compressions(
+                param_dict, epoch, [layer_id], n_batches, train_out)
+            compression_train, compression_val, layer_id_k1, name_k1 = out
+            layer_id_k1 = layer_id_k1[0]
+            name_k1 = name_k1[0]
+            compression_train = compression_train[0].tolist()
+            compression_val = compression_val[0].tolist()
+            d = {'epoch': epoch, 'compression': compression_train, 
+                 'layer_idx': layer_id_k1, 'layer_name': name_k1,
+                 'mode': 'train'}
+            d = {**d, **pd_mom}
+            ds.append(d)
     df = pd.DataFrame(ds)
     df['mode'] = df['mode'].astype("category")
     df['epoch'] = df['epoch'].astype(int)
-    df['layer_idx'] = df['layer_idx'].astype(int)
     return df
 
 
@@ -162,7 +195,8 @@ def get_compressions_over_training_batch(param_dict_list, epochs_idx=None,
 
 
 def get_compressions_over_layers(param_dict, epochs_idx,
-                                 layer_ids=slice(-1), n_batches=n_batches):
+                                 layer_ids=slice(-1), n_batches=n_batches,
+                                 projection=None, n_samples=None, mode='val'):
     train_out = train.train(param_dict)
     model, loader_train, loader_val, run_dir, pd_mom = train_out
     df = pd.DataFrame()
@@ -171,9 +205,16 @@ def get_compressions_over_layers(param_dict, epochs_idx,
         epochs = epochs[epochs_idx]
     ds = []
     for k1, epoch in enumerate(epochs):
-        out = get_compressions(param_dict, epoch, layer_ids, n_batches,
-                                      train_out)
-        compression_train, compression_val, layer_ids_k1, layer_names_k1 = out
+        if projection is not None:
+            out = get_dists_projected(param_dict, epoch, layer_ids,
+                                      n_batches, n_samples, mode, train_out)
+        else:
+            out = get_compressions(param_dict, epoch, layer_ids, n_batches,
+                                          train_out)
+        dists, layer_ids_k1, layer_names_k1 = out
+        # compression_train, compression_val, layer_ids_k1, layer_names_k1 = out
+        compression = dists[0] / dists[1]
+        compression_aligned = dists[2] / dists[3]
         filt = ~torch.isnan(compression_train)
         layer_names_k1 = [n for n, f in zip(layer_names_k1, filt) if f]
         layer_ids_k1 = [n for n, f in zip(layer_ids_k1, filt) if f]
@@ -183,16 +224,10 @@ def get_compressions_over_layers(param_dict, epochs_idx,
             # param_dict, epoch, layer_ids, n_batches)
         for k1, layer_id in enumerate(layer_ids_k1):
             layer_name = layer_names_k1[k1]
-            ct = compression_train[k1]
-            cv = compression_val[k1]
-            d = {'epoch': epoch, 'compression': ct,
-                 'layer_idx': layer_id, 'layer_name': layer_name,
-                 'mode': 'train'}
-            d = {**d, **pd_mom}
-            ds.append(d)
-            d = {'epoch': epoch, 'compression': cv, 
-                 'layer_idx': layer_id, 'layer_name': layer_name,
-                 'mode': 'val'}
+            ct = compression[k1]
+            d = dict(epoch=epoch, compression=compression,
+                     compression_aligned=compression_aligned,
+                     layer_idx=layer_id, layer_name=layer_name, mode=mode)
             d = {**d, **pd_mom}
             ds.append(d)
     df = pd.DataFrame(ds)
@@ -247,9 +282,14 @@ if __name__ == '__main__':
         # df = get_compressions_over_layers(ps, [0, -1])
         # get_compressions_over_training(ps, epochs_idx=[0, 5, 10, 20 -1])
     # print(run_num)
+    run_num=1
     ps = ps_all[run_num-1]
     df = get_compressions_over_layers(ps, [0, -1])
-    df2 = get_compressions_over_training(ps, epochs_idx=[0, 5, 10, 20 -1])
+    # df = get_compressions_over_layers(ps, [0, -1], projection='s')
+    # df = get_compressions_over_training(ps, epochs_idx = [0, -1], projection='s')
+    breakpoint()
+    # df = get_compressions_over_layers(ps, [0, -1])
+    # df2 = get_compressions_over_training(ps, epochs_idx=[0, 5, 10, 20 -1])
     # fn(ps_all[run_num-1])
     # df = get_compressions_over_training_batch(ps_all, epochs_idx=[0, 5, 10, 20 -1])
     # df = get_compressions_over_training(ps_all[0], epochs_idx=[0, 5, 10, 20 -1])
